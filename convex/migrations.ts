53e7d53cb86a574d52ab3f142c7f9d2d32a05268
seansusmilch <seantsusmilch@gmail.com>
2025-08-26 23:12:13 -0500

    feat: add migration for inference predictions; implement upsert logic to handle prediction data during migration, enhancing data integrity and management

--- convex/migrations.ts
+++ convex/migrations.ts
@@ -1,7 +1,30 @@
 import { Migrations } from '@convex-dev/migrations';
 import { components, internal } from './_generated/api.js';
 import type { DataModel } from './_generated/dataModel.js';
+import { RoboflowPrediction } from './lib/roboflow.js';
 
 export const migrations = new Migrations<DataModel>(components.migrations);
 
-export const runAll = migrations.runner([]);
+export const migratePredictions = migrations.define({
+  table: 'inferences',
+  migrateOne: async (ctx, doc) => {
+    const predictions: RoboflowPrediction[] = doc.response.predictions;
+    const predictionIds = await Promise.all(
+      predictions.map(async (prediction) => {
+        return await ctx.runMutation(internal.inference_predictions.upsert, {
+          inferenceId: doc._id,
+          prediction,
+        });
+      })
+    );
+    console.log('[migratePredictions] upserted predictions', {
+      inferenceId: doc._id,
+      predictionIds,
+    });
+    return doc;
+  },
+});
+
+export const runAll = migrations.runner([
+  internal.migrations.migratePredictions,
+]);
