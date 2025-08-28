19b967bc197872321f5b81db87e717b088e00a9c
seansusmilch <seantsusmilch@gmail.com>
2025-08-27 21:57:13 -0500

    feat: implement migrations to remove swimming pool inferences, predictions, and feedback submissions; enhance data integrity by filtering out unwanted classes from the database

--- convex/migrations.ts
+++ convex/migrations.ts
@@ -1,7 +1,7 @@
 import { Migrations } from '@convex-dev/migrations';
 import { components, internal } from './_generated/api.js';
 import type { DataModel } from './_generated/dataModel.js';
-import { RoboflowPrediction } from './lib/roboflow.js';
+import { RoboflowPrediction, RoboflowResponse } from './lib/roboflow.js';
 import { pointToTile } from './lib/tiles.js';
 
 export const migrations = new Migrations<DataModel>(components.migrations);
@@ -34,7 +34,43 @@ export const migrateScansCenterTile = migrations.define({
   },
 });
 
+export const removeSwimmingPoolsInferences = migrations.define({
+  table: 'inferences',
+  migrateOne: async (ctx, doc) => {
+    const response: RoboflowResponse = doc.response;
+    const predictions: RoboflowPrediction[] = response.predictions;
+    const filteredPredictions = predictions.filter(
+      (prediction) => prediction.class !== 'swimming-pool'
+    );
+
+    response.predictions = Array.from(filteredPredictions);
+    return { response };
+  },
+});
+
+export const removeSwimmingPoolPredictions = migrations.define({
+  table: 'inference_predictions',
+  migrateOne: async (ctx, doc) => {
+    if (doc.class !== 'swimming-pool') return;
+    await ctx.db.delete(doc._id);
+  },
+});
+
+export const removeSwimmingPoolFeedback = migrations.define({
+  table: 'feedback_submissions',
+  migrateOne: async (ctx, doc) => {
+    const predictionId = doc.predictionId;
+    const prediction = await ctx.db.get(predictionId);
+    if (prediction?.class !== 'swimming-pool') return;
+
+    await ctx.db.delete(doc._id);
+  },
+});
+
 export const runAll = migrations.runner([
-  internal.migrations.migratePredictions,
-  internal.migrations.migrateScansCenterTile,
+  // internal.migrations.migratePredictions,
+  // internal.migrations.migrateScansCenterTile,
+  internal.migrations.removeSwimmingPoolsInferences,
+  internal.migrations.removeSwimmingPoolPredictions,
+  internal.migrations.removeSwimmingPoolFeedback,
 ]);
